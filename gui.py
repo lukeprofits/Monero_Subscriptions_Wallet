3bd6500621cf11cabe05a496a171d816d9dde071
Luke Whittemore <luke.whittemore@mm-whittemorel.peace.daveramsey.com>
2024-02-22 14:46:11 -0600

    really ugly, but I arranged it a bit

--- gui.py
+++ gui.py
@@ -6,31 +6,41 @@ customtkinter.set_default_color_theme("monero_theme.json")
 class App(customtkinter.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.geometry("300x800")
+        self.geometry("500x200")
 
-        self.recieve_button = customtkinter.CTkButton(self, text="Recieve", command=self.open_recieve)
-        self.recieve_button.pack(side="top", padx=20, pady=20)
-
-        self.send_button = customtkinter.CTkButton(self, text="Send", command=self.open_send)
-        self.send_button.pack(side="top", padx=20, pady=20)
-
-        self.subscriptions_button = customtkinter.CTkButton(self, text="My Subscriptions", command=self.open_subscriptions)
-        self.subscriptions_button.pack(side="top", padx=20, pady=20)
+        self.sync_status = customtkinter.CTkLabel(self, text="( Sync Status )")
+        self.sync_status.place(relx=0.5, rely=0.125, anchor="center") #.pack(side="top", anchor="n", padx=20, pady=20)
 
         self.settings_button = customtkinter.CTkButton(self, text="Settings", command=self.open_settings)
-        self.settings_button.pack(side="top", padx=20, pady=20)
+        self.settings_button.place(relx=0.85, rely=0.125, anchor="center") #.pack(anchor="ne", padx=20, pady=20)
+
+        self.amount = customtkinter.CTkLabel(self, text="$150.00 USD", font=("Helvetica", 48))
+        self.amount.place(relx=0.5, rely=0.355, anchor="center") #.pack(padx=20, pady=20)
+
+        # CURRENCY SELECTOR DROPDOWN
+
+        self.recieve_button = customtkinter.CTkButton(self, text="Recieve", command=self.open_recieve)
+        self.recieve_button.place(relx=0.333, rely=0.685, anchor="center") #.pack(side="top", padx=20, pady=20)
+
+        self.pay_button = customtkinter.CTkButton(self, text="Pay", command=self.open_pay)
+        self.pay_button.place(relx=0.666, rely=0.685, anchor="center") #.pack(side="top", padx=20, pady=20)
+
+        self.subscriptions_button = customtkinter.CTkButton(self, text="                    Manage Subscriptions                    ", command=self.open_subscriptions)
+        self.subscriptions_button.place(relx=0.5, rely=0.875, anchor="center") #.pack(side="top", padx=20, pady=20)
+
+
 
         self.node_selection_button = customtkinter.CTkButton(self, text="Node Selection", command=self.open_node_selection)
-        self.node_selection_button.pack(side="top", padx=20, pady=20)
+        #self.node_selection_button.pack(side="top", padx=20, pady=20)
 
         self.welcome_message_button = customtkinter.CTkButton(self, text="Welcome Message", command=self.open_welcome_message)
-        self.welcome_message_button.pack(side="top", padx=20, pady=20)
+        #self.welcome_message_button.pack(side="top", padx=20, pady=20)
 
         self.add_payment_request_button = customtkinter.CTkButton(self, text="Add Monero Payment Request", command=self.open_add_payment_request)
-        self.add_payment_request_button.pack(side="top", padx=20, pady=20)
+        #self.add_payment_request_button.pack(side="top", padx=20, pady=20)
 
         self.manually_create_payment_request_button = customtkinter.CTkButton(self, text="Manually Create Monero Payment Request", command=self.open_manually_create_payment_request)
-        self.manually_create_payment_request_button.pack(side="top", padx=20, pady=20)
+        #self.manually_create_payment_request_button.pack(side="top", padx=20, pady=20)
 
         self.toplevel_window = None
 
@@ -40,11 +50,15 @@ class App(customtkinter.CTk):
         else:
             self.toplevel_window.focus()  # if window exists focus it
 
-    def open_send(self):
+    def open_pay(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = Send(self)  # create window if its None or destroyed
+            self.toplevel_window = Pay(self)  # create window if its None or destroyed
         else:
             self.toplevel_window.focus()  # if window exists focus it
+
+
+    # MANAGE SUBSCRIPTIONS
+
 
     def open_subscriptions(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
@@ -92,12 +106,12 @@ class Recieve(customtkinter.CTkToplevel):
         self.label.pack(padx=20, pady=20)
 
 
-class Send(customtkinter.CTkToplevel):
+class Pay(customtkinter.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("400x300")
 
-        self.label = customtkinter.CTkLabel(self, text="Send Window")
+        self.label = customtkinter.CTkLabel(self, text="Pay Window")
         self.label.pack(padx=20, pady=20)
 
 
