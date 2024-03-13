ed71c3867c004ca40fe2b4183afe459f57485625
Luke Whittemore <luke.whittemore@mm-whittemorel.peace.daveramsey.com>
2024-03-13 15:45:06 -0500

    populate set_currency window. Has issues, but better than being empty

--- src/views/set_currency.py
+++ src/views/set_currency.py
@@ -8,23 +8,53 @@ class SetCurrencyView(View):
         self._app = app
 
     def build(self):
-        self._app.geometry(cfg.RECEIVE_VIEW_GEOMETRY)
+        self._app.geometry(cfg.SET_CURRENCY_VIEW_GEOMETRY)
+
+        def default_currency_selector_callback(choice):
+            print(cfg.DEFAULT_CURRENCY)
+            cfg.DEFAULT_CURRENCY = choice
+            print("User chose:", choice)
+            print("Now set to:", cfg.DEFAULT_CURRENCY)
+
+        def secondary_currency_selector_callback(choice):
+            print(cfg.SECONDARY_CURRENCY)
+            print("User chose:", choice)
+            cfg.SECONDARY_CURRENCY = choice
+            print("Now set to:", cfg.SECONDARY_CURRENCY)
+
+        SET_CURRENCY_TEXT = """
+                Select the currencies that you would like for your wallet balance to be displayed in. 
+
+                Primary will be shown by default. Click the amount shown to toggle to secondary."""
+
 
         # Title
-        label = self.add(ctk.CTkLabel(self._app, text=' Set Currency:'))
+        label = self.add(ctk.CTkLabel(self._app, text='Set Currencies:'))
         label.grid(row=0, column=0, columnspan=3, padx=10, pady=5, sticky="ew")
 
         # Back Button
         back_button = self.add(ctk.CTkButton(self._app, text=cfg.BACK_BUTTON_EMOJI, font=(cfg.font, 24), width=35, height=30, command=self.open_main))
         back_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-        # Wallet input box
-        # Documentation: https://customtkinter.tomschimansky.com/documentation/widgets/entry
-        #input_box_for_wallet_or_request = self.add(ctk.CTkEntry(self._app, placeholder_text="Enter a monero payment request or wallet address..."))
-        #input_box_for_wallet_or_request.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="ew")
+        text = ctk.CTkLabel(self._app, text=SET_CURRENCY_TEXT)
+        text.grid(row=1, column=0, columnspan=3, padx=20, pady=20, sticky="ew")
 
-        copy_wallet_button = self.add(ctk.CTkButton(self._app, text="Copy Wallet Address", command=self.copy_wallet_address))
-        copy_wallet_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky="ew")
+        # Labels
+        label1 = self.add(ctk.CTkLabel(self._app, text='Default:'))
+        label1.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+
+        label2 = self.add(ctk.CTkLabel(self._app, text='Secondary:'))
+        label2.grid(row=2, column=2, padx=10, pady=5, sticky="ew")
+
+        # Default Currency
+        selected_currency = ctk.StringVar(value=cfg.DEFAULT_CURRENCY)
+        currency_selector = ctk.CTkOptionMenu(self._app, values=cfg.CURRENCY_OPTIONS, command=default_currency_selector_callback, variable=selected_currency)
+        currency_selector.grid(row=3, column=0, columnspan=1, padx=20, pady=20)
+
+        # Secondary Currency
+        selected_currency = ctk.StringVar(value=cfg.SECONDARY_CURRENCY)
+        currency_selector = ctk.CTkOptionMenu(self._app, values=cfg.CURRENCY_OPTIONS, command=secondary_currency_selector_callback, variable=selected_currency)
+        currency_selector.grid(row=3, column=2, columnspan=1, padx=20, pady=20)
 
         return self
 
@@ -33,45 +63,3 @@ class SetCurrencyView(View):
 
     def copy_wallet_address(self):
         self._app.switch_view('main')  # TODO: UPDATE THIS TO WORK!!!
-
-    # This was the old code for that window
-    '''
-    class SetCurrency(ctk.CTkToplevel):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.geometry("600x300")
-
-            def default_currency_selector_callback(choice):
-                global DEFAULT_CURRENCY
-                print(DEFAULT_CURRENCY)
-                DEFAULT_CURRENCY = choice
-                print("User chose:", choice)
-                print("Now set to:", DEFAULT_CURRENCY)
-
-            def secondary_currency_selector_callback(choice):
-                global SECONDARY_CURRENCY
-                print(SECONDARY_CURRENCY)
-                print("User chose:", choice)
-                SECONDARY_CURRENCY = choice
-                print("Now set to:", SECONDARY_CURRENCY)
-
-            set_currency_window_text = """
-            Set Default Currency:
-
-            The currency that you select will be shown by default. 
-
-            To toggle to the the Monero amount in the main window, simply click it."""
-
-            self.label = ctk.CTkLabel(self, text=set_currency_window_text)
-            self.label.pack(padx=20, pady=20)
-
-            # Default Currency
-            self.selected_currency = ctk.StringVar(value=DEFAULT_CURRENCY)
-            self.currency_selector = ctk.CTkOptionMenu(self, values=CURRENCY_OPTIONS, command=default_currency_selector_callback, variable=self.selected_currency)
-            self.currency_selector.pack(padx=20, pady=20)
-
-            # Secondary Currency
-            self.selected_currency = ctk.StringVar(value=SECONDARY_CURRENCY)
-            self.currency_selector = ctk.CTkOptionMenu(self, values=CURRENCY_OPTIONS, command=secondary_currency_selector_callback, variable=self.selected_currency)
-            self.currency_selector.pack(padx=20, pady=20)    
-    '''
