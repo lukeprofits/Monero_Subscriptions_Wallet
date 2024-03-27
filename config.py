1a67dcaf122c0197513f7e4a93d7cf6b10d80f2c
Luke Whittemore <luke.whittemore@mm-whittemorel.peace.daveramsey.com>
2024-03-27 07:43:54 -0500

    clean up

--- config.py
+++ config.py
@@ -374,9 +374,7 @@ def get_value(currency_ticker, usd_value):
         url = 'https://www.goldback.com/exchange-rate'
         page_content = requests.get(url).text
         # Find the gb_average_exchange_rate constant's value
-        match = re.search(r"gb_average_exchange_rate\s*=\s*'([\d.]+)';", page_content)
-
-        usd_cost_for_one_goldback = match.group(1)
+        usd_cost_for_one_goldback = re.search(r"gb_average_exchange_rate\s*=\s*'([\d.]+)';", page_content).group(1)
         dollar_value_in_goldbacks = Decimal(1) / Decimal(usd_cost_for_one_goldback)
         return dollar_value_in_goldbacks
 
