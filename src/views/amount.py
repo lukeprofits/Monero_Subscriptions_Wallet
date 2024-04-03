cd02c749347c7afb7db47c08a7098f3c8ebc5f39
lukeprofits <lukewhitepsp@gmail.com>
2024-04-02 22:24:24 -0400

    fixed one I missed

--- src/views/amount.py
+++ src/views/amount.py
@@ -7,21 +7,16 @@ class AmountView(View):
     def build(self):
         self._app.geometry(cfg.AMOUNT_VIEW_GEOMETRY)
 
-        # Title
-        label = self.add(ctk.CTkLabel(self._app, text=' Amount:', font=cfg.HEADINGS_FONT_SIZE))
-        label.grid(row=0, column=0, columnspan=3, padx=10, pady=5, sticky="ew")
-
-        # Back Button
-        back_button = self.add(ctk.CTkButton(self._app, text=cfg.BACK_BUTTON_EMOJI, font=(cfg.font, 24), width=35, height=30, command=self.open_main))
-        back_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-
-        # Wallet
-        wallet = self.add(ctk.CTkLabel(self._app, text=f'Sending to wallet: {cfg.SEND_TO_WALLET[:5]}...{cfg.SEND_TO_WALLET[-5:]}'))  # TODO: Make it so that they can click the wallet to go back to "pay" view
-        wallet.grid(row=1, column=0, columnspan=3, padx=10, pady=5, sticky="ew")
+        # Back button and title
+        cfg.back_and_title(self, ctk, cfg, title=' Amount:')
 
         # Input box
         self.input_box_for_amount = self.add(ctk.CTkEntry(self._app, placeholder_text="Enter an amount"))
-        self.input_box_for_amount.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky="ew")
+        self.input_box_for_amount.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="ew")
+
+        # Wallet
+        wallet = self.add(ctk.CTkLabel(self._app, text=f'To Wallet: {cfg.SEND_TO_WALLET[:5]}...{cfg.SEND_TO_WALLET[-5:]}'))  # TODO: Make it so that they can click the wallet to go back to "pay" view
+        wallet.grid(row=2, column=0, columnspan=3, padx=10, pady=5, sticky="ew")
 
         # Send button
         send_button = self.add(ctk.CTkButton(self._app, text="Send", command=self.send_button))
