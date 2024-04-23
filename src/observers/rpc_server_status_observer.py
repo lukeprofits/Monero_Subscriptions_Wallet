8d06bc99a97d9417a7464903cff616a48584d310
landlessdrunk <admin@drnk.org>
2024-04-22 20:46:31 -0500

    Removing unused observer.

--- src/observers/rpc_server_status_observer.py
+++
@@ -1,8 +0,0 @@
-from src.interfaces.observer import Observer
-
-class RPCServerStatusObserver(Observer):
-    def __init__(self, label):
-        self.label = label
-
-    def update(self, subject):
-        self.label.configure(text=subject.status_message)
