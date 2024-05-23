0fe7009d9eb3e35b11949276b13d2d10de756c9d
Luke Profits <lukewhitepsp@gmail.com>
2024-05-23 19:24:20 -0400

    adjust set_currency and amount views

--- src/views/set_currency.py
+++ src/views/set_currency.py
@@ -9,6 +9,7 @@ from config import default_currency, secondary_currency
 class SetCurrencyView(View):
     def build(self):
         self._app.geometry(styles.SET_CURRENCY_VIEW_GEOMETRY)
+        #self._app.columnconfigure([0, 1, 2, 3], weight=1)
 
         def default_currency_selector_callback(choice):
             cfg.config_file.set(section='subscriptions', option='default_currency', value=choice)
@@ -25,21 +26,21 @@ class SetCurrencyView(View):
 
         # Labels
         label1 = self.add(ctk.CTkLabel(self._app, text='Default:'))
-        label1.grid(row=2, column=0, padx=10, pady=(15, 5), sticky="ew")
+        label1.grid(row=2, column=0, padx=(120, 10), pady=(20, 5), sticky="ew")
 
         label2 = self.add(ctk.CTkLabel(self._app, text='Secondary:'))
-        label2.grid(row=2, column=2, padx=10, pady=5, sticky="ew")
+        label2.grid(row=2, column=1, padx=(10, 120), pady=(20, 5), sticky="ew")
 
         # TODO: Without selected_currency commented out, the buttons don't work on subsequest frames.
         # Default Currency
         selected_currency = ctk.StringVar(value=default_currency())
         currency_selector = self.add(ctk.CTkOptionMenu(self._app, values=Exchange.options(), corner_radius=15, command=default_currency_selector_callback, variable=selected_currency))
-        currency_selector.grid(row=3, column=0, columnspan=1, padx=20, pady=(5, 35))
+        currency_selector.grid(row=3, column=0, columnspan=1, padx=(120, 20), pady=(5, 35))
 
         # Secondary Currency
         selected_currency = ctk.StringVar(value=secondary_currency())
         currency_selector = self.add(ctk.CTkOptionMenu(self._app, values=Exchange.options(), corner_radius=15, command=secondary_currency_selector_callback, variable=selected_currency))
-        currency_selector.grid(row=3, column=2, columnspan=1, padx=20, pady=(5, 35))
+        currency_selector.grid(row=3, column=1, columnspan=1, padx=(20, 120), pady=(5, 35))
 
         return self
 
